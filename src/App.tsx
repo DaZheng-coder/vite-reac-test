33565d25fb433fd32353f0b49833ccc4f1c63b06
zhengjunqin <zhengjunqin@shoplazza.com>
2025-04-13 14:04:58 +0800

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+import {Button}  from 'antd'
+import { useEffect } from 'react'
+import axios from 'axios'
+
+
+function App() {
+  useEffect(() => {
+    axios.get('/mock/api/getUsers').then((res)=>{
+      console.log(res);
+    })
+  }, [])
+
+  return (
+    <div>
+      <Button >1</Button>
+    </div>
+  )
+}
+
+export default App
