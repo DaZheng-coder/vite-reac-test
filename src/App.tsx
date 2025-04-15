560ccbe0edce5beb9dbeeaebf4233090eadc54eb
zhengjunqin <zhengjunqin@shoplazza.com>
2025-04-15 16:23:33 +0800

    feat: 添加用户选择器组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,55 @@
 
-import {Button}  from 'antd'
-import { useEffect } from 'react'
-import axios from 'axios'
+import UserSelector from './components/UserSelector'
+import { IUserData } from './components/UserSelector/type';
 
 
 function App() {
-  useEffect(() => {
-    axios.get('/mock/api/getUsers').then((res)=>{
-      console.log('*** res', res);
-    })
-  }, [])
-
   return (
-    <div>
-      {/* *** test */}
-      <Button >1</Button>
+    <div className='flex flex-col items-center justify-center h-screen gap-10'>
+      {/* 多选 */}
+      <div>多选</div>
+      <UserSelector 
+        onSelect={(selected) => {
+          console.log('*** selected', selected);
+        }} 
+        selectProps={{
+          mode: 'multiple',
+          allowClear: true,
+          placeholder: 'Please select',
+          style: { width: '500px' },
+        }}
+      />
+      {/* 单选 */}
+      <div>单选</div>
+      <UserSelector 
+        onSelect={(selected) => {
+          console.log('*** selected', selected);
+        }} 
+        selectProps={{
+          allowClear: true,
+          placeholder: 'Please select',
+          style: { width: '500px' },
+        }}
+      />
+      {/* 自定义 */}
+      <div>自定义</div>
+      <UserSelector<IUserData> 
+        config={{
+          fetchUrl: '/api/v1/get_user_list',
+          format: (item) => ({
+            label: item.name,
+            value: item.id,
+          })
+        }}
+        onSelect={(selected) => {
+          console.log('*** selected', selected);
+        }} 
+        selectProps={{
+          allowClear: true,
+          placeholder: 'Please select',
+          style: { width: '500px' },
+        }}
+      />
     </div>
   )
 }
---
+++ src/components/UserSelector/index.tsx
@@ -0,0 +1,93 @@
+import { Select, SelectProps } from "antd";
+import axios from "axios";
+import { useCallback, useEffect, useRef, useState } from "react";
+
+/**
+ * 用户基础数据结构
+ */
+export interface IBaseUserData {
+  id: string;
+  name: string;
+  user: string;
+}
+
+/**
+ * 用户信息配置项
+ */
+export interface IConfig<T> {
+  fetchUrl: string;
+  format: (userList: T) => ISelectItem;
+}
+
+interface IUserSelectorProps<T> {
+  onSelect?: (selectUsers: T[]) => void;
+  selectProps?: SelectProps
+  config?: IConfig<T>;
+}
+
+
+/**
+ * antd 选项结构
+ */
+interface ISelectItem { label: string, value: string }
+
+const UserSelector= <T extends IBaseUserData,>(props: IUserSelectorProps<T>) => {
+  const{onSelect, config, selectProps = {}} = props
+  const userMapRef = useRef<Map<string, T>>(new Map()); // 映射用户源数据
+  const [selectList, setSelectList] = useState<ISelectItem[]>([]); // 下拉列表数据
+  const [loading, setLoading] = useState<boolean>(false);
+
+
+  /**
+   * 获取选中用户
+   */
+  const handleChange = useCallback((selected: string | string[]) => {
+    const selectedIds = Array.isArray(selected) ? selected : [selected];
+    const selectUsers = selectedIds.map((id) => {
+      return userMapRef.current.get(id);
+    })
+    onSelect?.(selectUsers as T[]);
+  }, [onSelect]);
+
+  /**
+   * 获取用户列表
+   */
+  const getUsers = useCallback( async () => {
+    setLoading(true);
+    try {
+      userMapRef.current.clear();
+      const userList = await axios.get(config?.fetchUrl || '/api/v1/get_user_list');
+      const selectData = userList.data.map((item: T) => {
+        userMapRef.current.set(item.id, item);
+        if (config?.format) {
+          return config.format(item)
+        }
+        return {
+          label: `${item.name} (${item.user})`,
+          value: item.id,
+        }
+      })
+      setSelectList(selectData);
+    } catch (error) {
+      console.log('getUsers error', error);
+    } finally {
+      setLoading(false)
+    }
+  }, [config])
+
+  useEffect(() => {
+    getUsers()
+  }, [getUsers])
+
+  return (
+    <Select
+      style={{ width: 200 }}
+      onChange={handleChange}
+      options={selectList}
+      loading={loading}
+      {...selectProps}
+    />
+  );
+}
+
+export default UserSelector
